126a285d12f8a361490fd01fdbace4c45ce986f2
IagoOlraSantos <iago.olra.santos@gmail.com>
2023-05-03 07:40:57 -0300

    adicionando miragejs para backend fake

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,35 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/globals";
 
+import { createServer } from "miragejs";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "Desenvolvimento de website",
+          amount: 12000,
+          type: "deposit",
+          category: "Desenvolvimento",
+          createdAt: new Date(),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          amount: -400,
+          type: "withdraw",
+          category: "Casa",
+          createdAt: new Date(),
+        },
+      ];
+    });
+  },
+});
+
 export function App() {
   return (
     <>
